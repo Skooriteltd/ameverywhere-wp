e85d05065158ed7773e0cecf3740771a3cf293f7
Craftitec <unitimarket@outlook.com>
2026-09-21 19:10:09 +0100

    build: Update compiled React assets for accessibility fixes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => '82c61f10b004d50269cd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => 'c184c2c2d584a64b5ee1');
